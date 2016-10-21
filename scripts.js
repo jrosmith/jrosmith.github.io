291f03959d73d6dbece2ed7d2dd305411297efa6
Joshua Smith <smithjoshr@gmail.com>
2016-10-21 11:04:30 -0400

    Import jQuery library, initialize clickHandler)

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
-const clickHandler = () => {
-  alert("clickHandler working");
+const toggleShow = ($el) => {
+  $('.show').addClass('hide');
+  $('.show').removeClass('show');
+
+  $el.removeClass('hide');
+  $el.addClass('show');
 };
 
-$('.meat-links').on('click', 'li', clickHandler);
+const clearDetail = ($el) => {
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const clickHandler = (e) => {
+    console.log(e.currentTarget);
+    let $el = $(e.target);
+
+    if ($el.hasClass("hide")) {
+      toggleShow($el);
+    }
+  };
+
+  $('.meat-links').click("li", clickHandler);
+});
